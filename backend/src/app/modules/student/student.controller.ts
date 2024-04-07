8998053d224c2511675e83e7d222aa9621f00bb6
MostakemHossain <mostakememon123@gmail.com>
2024-04-07 12:41:35 +0600

    catchAsync function implement

--- backend/src/app/modules/student/student.controller.ts
+++ backend/src/app/modules/student/student.controller.ts
@@ -1,64 +1,41 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { StudentServices } from './student.service';
 
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentServices.getAllStudentFromDb();
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Students retrieved Succesfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentServices.getSingleStudenFromDb(
-      req.params.studentId,
-    );
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Student is retrieved Succesfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-const deleteStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentServices.deleteStudentFromDb(
-      req.params.studentId,
-    );
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Student is deleted Succesfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+const getAllStudents = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentServices.getAllStudentFromDb();
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Students retrieved Succesfully',
+    data: result,
+  });
+});
+const getSingleStudent = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentServices.getSingleStudenFromDb(
+    req.params.studentId,
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Student is retrieved Succesfully',
+    data: result,
+  });
+});
+const deleteStudent = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentServices.deleteStudentFromDb(
+    req.params.studentId,
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Student is deleted Succesfully',
+    data: result,
+  });
+});
 
 export const StudentController = {
   getAllStudents,
--- backend/src/app/modules/user/user.controller.ts
+++ backend/src/app/modules/user/user.controller.ts
@@ -1,28 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { UserServices } from './user.service';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student } = req.body;
+const createStudent = catchAsync(async (req: Request, res: Response) => {
+  const { password, student } = req.body;
 
-    const result = await UserServices.createStudentIntoDB(password, student);
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'Student Create Succesfully',
-      data: result,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+  const result = await UserServices.createStudentIntoDB(password, student);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Student Create Succesfully',
+    data: result,
+  });
+});
 export const UserController = {
   createStudent,
 };
