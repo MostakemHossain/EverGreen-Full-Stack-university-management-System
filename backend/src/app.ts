db9ef6fd2d4707e699238de83da3ff10fd2489a3
MostakemHossain <mostakememon123@gmail.com>
2024-04-07 12:24:09 +0600

    create index route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,16 +2,14 @@ import cors from 'cors';
 import express from 'express';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.routes';
+import router from './app/routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req, res) => {
   res.send({
