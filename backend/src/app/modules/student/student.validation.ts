526a16597b48b7f2a5a878d1643253ed548d08b9
MostakemHossain <mostakememon123@gmail.com>
2024-04-07 14:08:01 +0600

    validate request middleware added

--- backend/src/app/modules/student/student.validation.ts
+++ backend/src/app/modules/student/student.validation.ts
@@ -91,53 +91,53 @@ const localGuardianValidationSchema = z.object({
 });
 
 const createStudentValidationSchema = z.object({
-  id: z.string({
-    required_error: 'Id is required',
-    invalid_type_error: 'Id must be string',
+  body: z.object({
+    password: z.string(),
+    student: z.object({
+      name: studentNameValidationSchema,
+      gender: z.enum(['Male', 'Female', 'Other']),
+      dateOfBirth: z
+        .string({
+          required_error: 'Date of birth is required',
+          invalid_type_error: 'Date of birth must be a string',
+        })
+        .min(1),
+      contactNumber: z
+        .string({
+          required_error: 'Contact number is required',
+          invalid_type_error: 'Contact number must be a string',
+        })
+        .min(1),
+      emergencyContactNumber: z
+        .string({
+          required_error: 'Emergency contact number is required',
+          invalid_type_error: 'Emergency contact number must be a string',
+        })
+        .min(1),
+      email: z
+        .string({
+          required_error: 'Email must be valid.',
+          invalid_type_error: 'Email must be a string',
+        })
+        .email(),
+      bloodGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']),
+      presentAddress: z
+        .string({
+          required_error: 'Present address is required',
+          invalid_type_error: 'Present address must be a string',
+        })
+        .min(1),
+      permanentAddress: z
+        .string({
+          required_error: 'Permanent address is required',
+          invalid_type_error: 'Permanent address must be a string',
+        })
+        .min(1),
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
+      profileImage: z.string().optional(),
+    }),
   }),
-  name: studentNameValidationSchema,
-  gender: z.enum(['Male', 'Female', 'Other']),
-  dateOfBirth: z
-    .string({
-      required_error: 'Date of birth is required',
-      invalid_type_error: 'Date of birth must be a string',
-    })
-    .min(1),
-  contactNumber: z
-    .string({
-      required_error: 'Contact number is required',
-      invalid_type_error: 'Contact number must be a string',
-    })
-    .min(1),
-  emergencyContactNumber: z
-    .string({
-      required_error: 'Emergency contact number is required',
-      invalid_type_error: 'Emergency contact number must be a string',
-    })
-    .min(1),
-  email: z
-    .string({
-      required_error: 'Email must be valid.',
-      invalid_type_error: 'Email must be a string',
-    })
-    .email(),
-  bloodGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']),
-  presentAddress: z
-    .string({
-      required_error: 'Present address is required',
-      invalid_type_error: 'Present address must be a string',
-    })
-    .min(1),
-  permanentAddress: z
-    .string({
-      required_error: 'Permanent address is required',
-      invalid_type_error: 'Permanent address must be a string',
-    })
-    .min(1),
-  guardian: guardianValidationSchema,
-  localGuardian: localGuardianValidationSchema,
-  profileImage: z.string().optional(),
-  isDeleted: z.boolean().default(false),
 });
 
 export const StudentValidation = {
--- backend/src/app/modules/user/user.routes.ts
+++ backend/src/app/modules/user/user.routes.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import validateRequest from '../../middleware/validate.Request';
+import { StudentValidation } from '../student/student.validation';
 import { UserController } from './user.controller';
 const router = express.Router();
-router.post('/create-student', UserController.createStudent);
+
+router.post(
+  '/create-student',
+  validateRequest(StudentValidation.createStudentValidationSchema),
+  UserController.createStudent,
+);
 export const UserRoutes = router;
