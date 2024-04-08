d6342804182bdf1479c9ce81da5d4e39ed4c9edd
MostakemHossain <mostakememon123@gmail.com>
2024-04-08 23:26:03 +0600

    handle cast and duplicate error

--- backend/src/app/middleware/globalErrorHandler.ts
+++ backend/src/app/middleware/globalErrorHandler.ts
@@ -5,6 +5,8 @@ import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
 import config from '../config';
 import handleZodError from '../errors/HandleZodError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
 import handleValidationError from '../errors/handleValidationError';
 import { TErrorSource } from '../interface/error';
 
@@ -31,6 +33,16 @@ const globalErrorHandler = (
     errorSources = simplifiedError?.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
