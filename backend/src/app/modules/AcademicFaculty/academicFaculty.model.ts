3994ef8490677b3660654c0ce937bcdff63637de
MostakemHossain <mostakememon123@gmail.com>
2024-04-08 17:35:21 +0600

    some bug fix and modification

--- backend/src/app/modules/AcademicFaculty/academicFaculty.model.ts
+++ backend/src/app/modules/AcademicFaculty/academicFaculty.model.ts
@@ -14,4 +14,20 @@ const academicFacultySchema = new Schema<TAcademicFaculty>(
   },
 );
 
+academicFacultySchema.pre('save', async function (next) {
+  const isFacultyExists = await AcademicFaculty.findOne({ name: this.name });
+  if (isFacultyExists) {
+    throw new Error('This Faculty is Already Exists');
+  }
+  next();
+});
+academicFacultySchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isFacultyExists = await AcademicFaculty.find(query);
+  if (!isFacultyExists) {
+    throw new Error('This Faculty is Not found');
+  }
+  next();
+});
+
 export const AcademicFaculty = model('AcademicFaculty', academicFacultySchema);
--- backend/src/app/modules/student/student.model.ts
+++ backend/src/app/modules/student/student.model.ts
@@ -142,10 +142,15 @@ const studentSchema = new Schema<TStudent, TStudentModel>(
       type: localGuardianSchema,
       required: [true, 'Local guardian details are required'],
     },
-    admissionSemester:{
-      type:Schema.Types.ObjectId,
-      ref:'AcademicSemester'
+    admissionSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
     },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicDepartment',
+    },
+
     profileImage: {
       type: String,
       required: [true, 'Profile image is required'],
