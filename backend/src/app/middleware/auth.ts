c00cce0228c06e1b57d4efb537128ea2060fdae2
MostakemHossain <mostakememon123@gmail.com>
2024-04-12 20:28:19 +0600

    auth middleware update

--- backend/src/app/middleware/auth.ts
+++ backend/src/app/middleware/auth.ts
@@ -3,6 +3,7 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 import AppError from '../utils/AppError';
 import catchAsync from '../utils/catchAsync';
 
@@ -13,22 +14,34 @@ const auth = (...requiredRoles: TUserRole[]) => {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorizad');
     }
     // check the token is valid
-    jwt.verify(
+    const decoded = jwt.verify(
       token,
       config.jwt_access_serect as string,
-      function (err, decoded) {
-        if (err) {
-          throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorizad');
-        }
-        const role = (decoded as JwtPayload).role;
-        if (requiredRoles && !requiredRoles.includes(role)){
-            throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorizad');
-        }
-          req.user = decoded as JwtPayload;
+    ) as JwtPayload;
+    const { userId, role, iat } = decoded;
 
-        next();
-      },
-    );
+    // check the user is exists
+    const isUserExists = await User.findOne({ id: userId });
+
+    if (!isUserExists) {
+      throw new AppError(httpStatus.NOT_FOUND, 'User is not Found');
+    }
+    // check is the user is already deleted
+    if (isUserExists?.isDeleted || isUserExists.status === 'blocked') {
+      throw new AppError(httpStatus.NOT_FOUND, 'User is not Found');
+    }
+    const passwordChangeAt= isUserExists?.passwordChangeAt as string | number | Date
+    const passwordChangedAt =
+      new Date(passwordChangeAt).getTime() / 1000;
+    if (passwordChangedAt > Number(iat)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorizad');
+    }
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorizad');
+    }
+    req.user = decoded as JwtPayload;
+
+    next();
   });
 };
 export default auth;
--- backend/src/app/modules/Faculty/faculty.routes.ts
+++ backend/src/app/modules/Faculty/faculty.routes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import auth from '../../middleware/auth';
 import validateRequest from '../../middleware/validate.Request';
+import { USER_ROLE } from '../user/user.constant';
 import { FacultyController } from './faculty.controller';
 import { FacultyValidations } from './faculty.validation';
 const router = express.Router();
 
-router.get('/', auth(), FacultyController.getAllFaculty);
+router.get(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.faculty),
+  FacultyController.getAllFaculty,
+);
 router.get('/:facultyId', FacultyController.getSingleFaculty);
 router.patch(
   '/:facultyId',
