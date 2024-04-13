772f8a3539b690afd18d7c157950d629f2bc7bd6
MostakemHossain <mostakememon123@gmail.com>
2024-04-13 23:14:19 +0600

    main Layout setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-
+import MainLayout from "./components/layout/MainLayout";
 
 function App() {
-  return (
-    <>
-      <h1>University management system</h1>
-    </>
-  );
+  return <MainLayout />;
 }
 
 export default App;
---
+++ frontend/src/components/layout/MainLayout.tsx
@@ -0,0 +1,83 @@
+import { Layout, Menu, MenuProps } from "antd";
+
+const { Header, Content, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "3",
+    label: "User Management",
+    children: [
+      {
+        key: "11",
+        label: "Create Admin",
+      },
+      {
+        key: "12",
+        label: "Create Faculty",
+      },
+      {
+        key: "13",
+        label: "Create Student",
+      },
+    ],
+  },
+];
+
+const MainLayout = () => {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            textAlign: "center",
+            height: "4rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h1> Ever Green University</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            the main content should run here
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
